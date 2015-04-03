687fcfe2f0e731369f0fc135c9e2d9afec96a5b3
Eldan Goldenberg <eldang@gmail.com>
2015-04-03 13:38:57 -0700

    added combo query

--- command_reference.sql
+++ command_reference.sql
@@ -13,6 +13,7 @@ SELECT lines.*
 
 -- points by province
 -- need to filter on country and province name in case of province names that exist in more than one country
+-- may want to add a buffer to get n km around the province
 
 SELECT points.* 
 	FROM 
@@ -25,7 +26,9 @@ SELECT points.*
 				AND g.name_1 ilike 'Rift Valley';
 
 -- polygons by region
--- the st_buffer() hack was suggested by http://postgis.17.x6.nabble.com/How-to-trim-a-GeometryCollection-to-get-a-MultiPolygon-td3556351.html
+-- the st_buffer(the_geom,0) hack was suggested by http://postgis.17.x6.nabble.com/How-to-trim-a-GeometryCollection-to-get-a-MultiPolygon-td3556351.html
+-- it shouldn't break points or lines, but it may be rather slow
+-- of course, we can also use it to make sure our selections go a little beyond the boundaries. In that use, the number is a radius in metres (given SRID 4326).
 
 SELECT polygons.* 
 	FROM 
@@ -35,4 +38,88 @@ SELECT polygons.*
 			ON 
 				st_intersects(st_transform(polygons.way,4326), st_buffer(g.the_geom,0))
 				AND g.name ilike 'East & Horn of Africa';
+
+
+
+
+-- combining the data types
+-- 3 different geometry types, combined into one GEOMETRYCOLLECTION
+-- note that we have to add "NULL AS x" to each table's select query, where x is each field that exists in one of the other tables but not this one
+
+SELECT 
+	osm_id, access, "addr:housename", "addr:housenumber", "addr:interpolation", 
+ admin_level, aerialway, aeroway, amenity, area, barrier, bicycle, 
+ brand, bridge, boundary, building, capital, construction, covered, 
+ culvert, cutting, denomination, disused, ele, embankment, foot, 
+ "generator:source", harbour, highway, historic, horse, intermittent, 
+ junction, landuse, layer, leisure, lock, man_made, military, 
+ motorcar, name, "natural", office, oneway, operator, place, poi, 
+ population, power, power_source, public_transport, railway, ref, 
+ religion, route, service, shop, sport, surface, toll, tourism, 
+ "tower:type",
+ NULL AS tracktype,
+ tunnel, water, waterway, wetland, width, wood, 
+ z_order, 
+ NULL AS way_area,
+ ST_ForceCollection(way) AS geom
+	FROM 
+		public.africa_point as points
+		INNER JOIN 
+			ccrp.ccrp_provinces as g 
+			ON 
+				st_intersects(st_transform(points.way,4326), g.the_geom)
+				AND g.name_0 ilike 'Kenya'
+				AND g.name_1 ilike 'Rift Valley'
+UNION
+SELECT 
+	osm_id, access, "addr:housename", "addr:housenumber", "addr:interpolation", 
+	admin_level, aerialway, aeroway, amenity, area, barrier, bicycle, 
+	brand, bridge, boundary, building, 
+	NULL AS capital,
+	construction, covered, culvert, 
+	cutting, denomination, disused, 
+	NULL AS ele,
+	embankment, foot, "generator:source", 
+	harbour, highway, historic, horse, intermittent, junction, landuse, 
+	layer, leisure, lock, man_made, military, motorcar, name, "natural", 
+	office, oneway, operator, place, 
+	NULL AS poi, population, power, power_source, 
+	public_transport, railway, ref, religion, route, service, shop, 
+	sport, surface, toll, tourism, "tower:type", tracktype, tunnel, 
+	water, waterway, wetland, width, wood, z_order, way_area,
+	ST_ForceCollection(way) AS geom
+	FROM 
+		public.africa_line as lines
+		INNER JOIN 
+			ccrp.ccrp_provinces as g 
+			ON 
+				st_intersects(st_transform(lines.way,4326), g.the_geom)
+				AND g.name_0 ilike 'Kenya'
+				AND g.name_1 ilike 'Rift Valley'
+UNION
+SELECT 
+	osm_id, access, "addr:housename", "addr:housenumber", "addr:interpolation", 
+	admin_level, aerialway, aeroway, amenity, area, barrier, bicycle, 
+	brand, bridge, boundary, building, 
+	NULL AS capital,
+	construction, covered, culvert, 
+	cutting, denomination, disused, 
+	NULL AS ele,
+	embankment, foot, "generator:source", 
+	harbour, highway, historic, horse, intermittent, junction, landuse, 
+	layer, leisure, lock, man_made, military, motorcar, name, "natural", 
+	office, oneway, operator, place, 
+	NULL AS poi, population, power, power_source, 
+	public_transport, railway, ref, religion, route, service, shop, 
+	sport, surface, toll, tourism, "tower:type", tracktype, tunnel, 
+	water, waterway, wetland, width, wood, z_order, way_area,
+	ST_ForceCollection(way) AS geom
+	FROM 
+		public.africa_polygon as polygons
+		INNER JOIN 
+			ccrp.ccrp_provinces as g 
+			ON 
+				st_intersects(st_transform(polygons.way,4326), g.the_geom)
+				AND g.name_0 ilike 'Kenya'
+				AND g.name_1 ilike 'Rift Valley';
 
