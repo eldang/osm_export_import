98325b9b6539ea6f89a1195d9c16e5e03d9d293c
Eldan Goldenberg <eldang@gmail.com>
2015-04-03 14:19:50 -0700

    data simplification
    Honestly not sure this is worth doing, because it’s only a few fields.

--- command_reference.sql
+++ command_reference.sql
@@ -123,3 +123,112 @@ SELECT
 				AND g.name_0 ilike 'Kenya'
 				AND g.name_1 ilike 'Rift Valley';
 
+
+
+-- thinning the data
+
+-- remove poi field because it's always blank
+-- reduce the following fields to boolean because they only have one value:
+	-- TRUE or NULL:
+		-- culvert=yes
+	-- TRUE, FALSE or NULL		
+		-- intermittent= actually a range of values, but they reduce to boolean
+		-- lock=yes or lock=no
+		-- toll=no or toll= anything else is a yes
+
+SELECT 
+	osm_id, access, "addr:housename", "addr:housenumber", "addr:interpolation", 
+	admin_level, aerialway, aeroway, amenity, area, barrier, bicycle, 
+	brand, bridge, boundary, building, capital, construction, covered, 
+	CASE WHEN culvert='yes' THEN TRUE ELSE NULL::boolean END AS culvert, 
+	cutting, denomination, disused, ele, embankment, foot, 
+	"generator:source", harbour, highway, historic, horse, 
+	CASE WHEN intermittent='no' THEN FALSE WHEN intermittent=NULL THEN NULL::boolean ELSE TRUE END as intermittent,
+	junction, landuse, layer, leisure, 
+	CASE WHEN lock='yes' THEN TRUE WHEN lock='no' THEN FALSE ELSE NULL::boolean END AS lock, 
+	man_made, military, 
+	motorcar, name, "natural", office, oneway, operator, place, 
+	population, power, power_source, public_transport, railway, ref, 
+	religion, route, service, shop, sport, surface, 
+	CASE WHEN toll='no' THEN FALSE WHEN toll=NULL THEN NULL::boolean ELSE TRUE END as toll,
+	tourism, 
+	"tower:type",
+	NULL AS tracktype,
+	tunnel, water, waterway, wetland, width, wood, 
+	z_order, 
+	NULL AS way_area,
+	ST_ForceCollection(way) AS geom
+	FROM 
+		public.africa_point as points
+		INNER JOIN 
+			ccrp.ccrp_provinces as g 
+			ON 
+				st_intersects(st_transform(points.way,4326), g.the_geom)
+				AND g.name_0 ilike 'Kenya'
+				AND g.name_1 ilike 'Rift Valley'
+UNION
+SELECT 
+	osm_id, access, "addr:housename", "addr:housenumber", "addr:interpolation", 
+	admin_level, aerialway, aeroway, amenity, area, barrier, bicycle, 
+	brand, bridge, boundary, building, 
+	NULL AS capital,
+	construction, covered,
+	CASE WHEN culvert='yes' THEN TRUE ELSE NULL::boolean END AS culvert, 
+	cutting, denomination, disused, 
+	NULL AS ele,
+	embankment, foot, "generator:source", 
+	harbour, highway, historic, horse, 
+	CASE WHEN intermittent='no' THEN FALSE WHEN intermittent=NULL THEN NULL::boolean ELSE TRUE END as intermittent,
+	junction, landuse, 
+	layer, leisure,
+	CASE WHEN lock='yes' THEN TRUE WHEN lock='no' THEN FALSE ELSE NULL::boolean END AS lock, 
+	man_made, military, motorcar, name, "natural", 
+	office, oneway, operator, place, 
+	population, power, power_source, 
+	public_transport, railway, ref, religion, route, service, shop, 
+	sport, surface,
+	CASE WHEN toll='no' THEN FALSE WHEN toll=NULL THEN NULL::boolean ELSE TRUE END as toll,
+	tourism, "tower:type", tracktype, tunnel, 
+	water, waterway, wetland, width, wood, z_order, way_area,
+	ST_ForceCollection(way) AS geom
+	FROM 
+		public.africa_line as lines
+		INNER JOIN 
+			ccrp.ccrp_provinces as g 
+			ON 
+				st_intersects(st_transform(lines.way,4326), g.the_geom)
+				AND g.name_0 ilike 'Kenya'
+				AND g.name_1 ilike 'Rift Valley'
+UNION
+SELECT 
+	osm_id, access, "addr:housename", "addr:housenumber", "addr:interpolation", 
+	admin_level, aerialway, aeroway, amenity, area, barrier, bicycle, 
+	brand, bridge, boundary, building, 
+	NULL AS capital,
+	construction, covered,
+	CASE WHEN culvert='yes' THEN TRUE ELSE NULL::boolean END AS culvert, 
+	cutting, denomination, disused, 
+	NULL AS ele,
+	embankment, foot, "generator:source", 
+	harbour, highway, historic, horse, 
+	CASE WHEN intermittent='no' THEN FALSE WHEN intermittent=NULL THEN NULL::boolean ELSE TRUE END as intermittent,
+	junction, landuse, 
+	layer, leisure,
+	CASE WHEN lock='yes' THEN TRUE WHEN lock='no' THEN FALSE ELSE NULL::boolean END AS lock, 
+	man_made, military, motorcar, name, "natural", 
+	office, oneway, operator, place, 
+	population, power, power_source, 
+	public_transport, railway, ref, religion, route, service, shop, 
+	sport, surface,
+	CASE WHEN toll='no' THEN FALSE WHEN toll=NULL THEN NULL::boolean ELSE TRUE END as toll,
+	tourism, "tower:type", tracktype, tunnel, 
+	water, waterway, wetland, width, wood, z_order, way_area,
+	ST_ForceCollection(way) AS geom
+	FROM 
+		public.africa_polygon as polygons
+		INNER JOIN 
+			ccrp.ccrp_provinces as g 
+			ON 
+				st_intersects(st_transform(polygons.way,4326), g.the_geom)
+				AND g.name_0 ilike 'Kenya'
+				AND g.name_1 ilike 'Rift Valley';
